a0a526646da28440e49898b700cfb2627893d221
ayasiba <167832748+ayasiba@users.noreply.github.com>
2024-06-25 10:38:56 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,5 +1,8 @@
 alert('Hello,Aya!');
 alert(1+1);
 alert(Math.sqrt(1+1));
-alert(2+3)
-alert('2'+'3')
+alert(2+3);
+alert('2'+'3');
+alert(Number('2')+Number('3'));
+alert('2'+3);
+alert(Number('2')+3);
